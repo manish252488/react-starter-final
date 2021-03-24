669b0edacb88cced285832260c28ed7a031eb601
manish252488 <manish25.manish2488@gmail.com>
2021-03-24 16:34:13 +0530

    ipv6 disable

--- src/components/home/Untitled-1.sh
+++ src/components/home/Untitled-1.sh
@@ -2,3 +2,20 @@ git clone https://github.com/Und3rf10w/kali-anonsurf.git && cd kali-anonsurf/ &&
 ifconfig
 
 
+
+# hide ipv6 
+
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sysctl -w net.ipv6.conf.lo.disable_ipv6=1
+
+
+#permanent sol
+
+
+To permanently disable IPv6 on your Kali Linux system, you can modify the /etc/sysctl.conf file with your editor. Add the following 3 lines to the /etc/sysctl.conf configuration file:
+
+net.ipv6.conf.all.disable_ipv6=1
+net.ipv6.conf.default.disable_ipv6=1
+net.ipv6.conf.lo.disable_ipv6=1
+sysctl -p
