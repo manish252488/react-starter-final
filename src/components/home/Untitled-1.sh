ed2d1ee00cb36f7c54c9c295d8ac41831a7b046b
manish252488 <manish25.manish2488@gmail.com>
2021-03-24 15:49:26 +0530

    sh file added

---
+++ src/components/home/Untitled-1.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/Und3rf10w/kali-anonsurf.git && cd kali-anonsurf/ && ./installer.sh && anonsurf start && sudo apt install macchanger && sudo macchanger -r <network>
+ifconfig
+
+
